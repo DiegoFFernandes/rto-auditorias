87b8f9bd4fbd913130cc7da738684137693113bb
DiegoFFernandes <79951748+DiegoFFernandes@users.noreply.github.com>
2025-08-11 17:09:22 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const routes = require("./routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use("/api", routes);
 
