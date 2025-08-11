37c908d7e656ab47fa0cef7155a76eec1a81ac01
Diego Ferreira <ti.campina@ivorecap.com.br>
2025-08-11 16:04:34 -0300

    Mudandar no server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', routes);
+app.use("/api", routes);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
